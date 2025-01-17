4d4f1ca490547acf442ad06cc2e890dec2933e71
wogks <108786958+wogks@users.noreply.github.com>
2025-01-18 00:41:01 +0900

    Feat: deleting todo

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInPut = document.querySelector("#todo-form input");
 
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span")
-
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteTodo)
+    li.appendChild(span);
+    li.appendChild(button);
+
     toDoList.appendChild(li);
 
 }
